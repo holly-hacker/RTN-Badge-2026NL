44bbec62ad1650bc778d500138ccb22cd781e8dd
Variant9 <dario.db@hotmail.com>
2026-07-10 03:25:52 +0200

    Write LED driver, create test program for LEDs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
 #![no_main]
 
+mod ws2812b;
+
 use ch32_hal::Peri;
 use ch32_hal::gpio::{AnyPin, Level, Output};
 use embassy_executor::Spawner;
 use embassy_time::Timer;
 use panic_halt as _;
+
+use crate::ws2812b::Ws2812b;
 
 #[embassy_executor::task]
 async fn blink(pin: Peri<'static, AnyPin>, interval_ms: u64) {
@@ -21,14 +25,40 @@ async fn blink(pin: Peri<'static, AnyPin>, interval_ms: u64) {
 
 #[embassy_executor::main(entry = "ch32_hal::entry")]
 async fn main(spawner: Spawner) -> ! {
-    ch32_hal::debug::SDIPrint::enable();
-    let p = ch32_hal::init(ch32_hal::Config::default());
+    let config = ch32_hal::Config {
+        rcc: ch32_hal::rcc::Config::SYSCLK_FREQ_144MHZ_HSE,
+        ..Default::default()
+    };
+    let p = ch32_hal::init(config);
 
     spawner.spawn(blink(p.PB1.into(), 1000).unwrap());
-    loop {
-        Timer::after_millis(1000).await;
 
-        // TODO: the println macro blocks with no debugger connected, figure out a better way.
-        // ch32_hal::println!("tick");
+    let mut leds = ws2812b::Ws2812b::new(p.SPI1, p.PA7, p.DMA1_CH3).await;
+
+    let mut color_buffer = [ws2812b::Color::BLACK; 64];
+
+    // hot restarts don't clear the LED data, so explicitly clear them here
+    let reset_timer = leds.set_colors(&color_buffer).await;
+    reset_timer.await;
+
+    // give user a bit of time to restart int bootloader mode without having LEDs on
+    Timer::after_millis(500).await;
+
+    let mut prev_idx = 0;
+    let mut cur_idx = 0;
+    loop {
+        color_buffer[prev_idx] = ws2812b::Color::BLACK;
+        color_buffer[cur_idx] = ws2812b::Color::WHITE;
+
+        let reset_timer = leds.set_colors(&color_buffer).await;
+        let interval_timer = Timer::after_millis(50);
+
+        // both timers have started, so we're waiting in parallel
+        reset_timer.await;
+        interval_timer.await;
+
+        prev_idx = cur_idx;
+        cur_idx += 1;
+        cur_idx %= Ws2812b::COLOR_COUNT;
     }
 }
---
+++ src/ws2812b.rs
@@ -0,0 +1,125 @@
+use core::{
+    hint::unreachable_unchecked,
+    mem::{MaybeUninit, transmute},
+};
+
+use ch32_hal::{
+    Peri,
+    peripherals::{self, DMA1_CH3, PA7, SPI1},
+    spi::{BitOrder, Config, Spi},
+    time::Hertz,
+};
+use embassy_time::Timer;
+
+/// The SPI frequency, chosen based on the timing requirements of the LEDs on the board.
+///
+/// WS2812 LEDs communicate by sending either a short high and long low pulse (0 code) or a long
+/// high and short low pulse (1 code). The timing depends on the model, but WS2812B-2020 LEDs
+/// require 220ns-380ns for a short pulse and 580ns-1000ns for a long pulse.
+///
+/// For our purposes, it is most efficient to choose a long pulse duration that is 2x (for memory
+/// usage) or 3x (for speed/simplicity) the duration of the short pulse, as this means we need less
+/// SPI transfers/clocks to send the same pulses. This narrows the duration range for the short
+/// pulse down to 290ns-380ns and 220ns-333ns respectively.
+///
+/// Additionally, on the CH32V203, the SPI frequency must be an integer division of the system
+/// clock, which is configured as 144MHz (the maximum available). We also prefer higher frequencies
+/// as this means transfers take less time. A division of 32 (4.5MHz) results in a clock interval of
+/// 222.22ns, with a long pulse of 666.67ns at 3x that duration.
+const SPI_FREQ: u32 = 4_500_000;
+
+pub struct Color {
+    // NOTE: order should probably be optimized?
+    g: u8,
+    r: u8,
+    b: u8,
+}
+
+impl Color {
+    pub const fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+
+    pub const fn get_components(&self) -> [u8; 3] {
+        [self.g, self.r, self.b]
+    }
+}
+
+// color constants
+impl Color {
+    #[allow(unused)]
+    pub const BLACK: Color = Color::new(0, 0, 0);
+    #[allow(unused)]
+    pub const WHITE: Color = Color::new(255, 255, 255);
+}
+
+pub struct Ws2812b {
+    spi: Spi<'static, peripherals::SPI1, ch32_hal::mode::Async>,
+}
+
+impl Ws2812b {
+    /// How many colors should be sent.
+    pub const COLOR_COUNT: usize = 8 * 8;
+
+    pub async fn new(
+        peri: Peri<'static, SPI1>,
+        mosi: Peri<'static, PA7>,
+        dma: Peri<'static, DMA1_CH3>,
+    ) -> Self {
+        let mut config = Config::default();
+        config.bit_order = BitOrder::MsbFirst;
+        config.frequency = Hertz::hz(SPI_FREQ);
+
+        // Don't actually use SPI. We just use the peripheral to bitbang the protocol, meaning we
+        // only need 1 pin to be configured.
+        let mut spi = Spi::new_txonly_nosck(peri, mosi, dma, config);
+
+        // ensure TX pin is not floating and ensure LED is not trying to read the bogus data
+        _ = spi.write(&[0_u8]).await;
+        Timer::after_micros(280).await;
+
+        Self { spi }
+    }
+
+    pub async fn set_colors(&mut self, colors: &[Color; Self::COLOR_COUNT]) -> embassy_time::Timer {
+        // Each color requires 24 codes (8 per channel) and each code requires 4 transfers, meaning
+        // 12 bytes per color. For an 8x8 matrix, this means a 768 byte DMA buffer.
+        // TODO: this probably does not belong on the stack
+        let mut dma_buffer = [const { MaybeUninit::<u8>::uninit() }; Self::COLOR_COUNT * 3 * 4];
+
+        for (color_i, color) in colors.iter().enumerate() {
+            let color_start = color_i * 3 * 4;
+
+            for (component_i, component) in color.get_components().into_iter().enumerate() {
+                let component_start = color_start + component_i * 4;
+
+                #[allow(clippy::identity_op)]
+                dma_buffer[component_start + 0].write(Self::get_double_bit_pattern(component, 6));
+                dma_buffer[component_start + 1].write(Self::get_double_bit_pattern(component, 4));
+                dma_buffer[component_start + 2].write(Self::get_double_bit_pattern(component, 2));
+                dma_buffer[component_start + 3].write(Self::get_double_bit_pattern(component, 0));
+            }
+        }
+
+        // SAFETY: just initialized each byte
+        // TODO: use MaybeUninit::transpose once it stabilizes, see rust-lang/rust#96097
+        let dma_buffer: [u8; Self::COLOR_COUNT * 3 * 4] = unsafe { transmute(dma_buffer) };
+
+        // NOTE: implementation returns no errors
+        _ = self.spi.write(&dma_buffer).await;
+
+        // return delay that resolves after
+        embassy_time::Timer::after_micros(280)
+    }
+
+    const fn get_double_bit_pattern(num: u8, idx: usize) -> u8 {
+        match (num >> idx) & 0b11 {
+            0b00 => 0b1000_1000,
+            0b01 => 0b1000_1110,
+            0b10 => 0b1110_1000,
+            0b11 => 0b1110_1110,
+            // SAFETY: bitwise mask verifies this
+            _ => unsafe { unreachable_unchecked() },
+        }
+    }
+}
